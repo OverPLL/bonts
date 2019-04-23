9d319a40e39332942dbfc662d4d82e3151e32920
OverPLL <40733893+OverPLL@users.noreply.github.com>
2019-04-23 15:57:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -136,4 +136,4 @@ async def _eval(ctx, *, body):
     
 if not os.environ.get('NTcwMDIwMDk5ODg5ODIzNzQ1.XL8HKg.xoNsBWj1cVhyN3gMz34gVFb2EdE'):
     print("no token found REEEE!")
-bot.run(os.environ.get('NTcwMDIwMDk5ODg5ODIzNzQ1.XL8HKg.xoNsBWj1cVhyN3gMz34gVFb2EdE').strip('"'), bot=False) 
+bot.run(os.environ.get('NTcwMDIwMDk5ODg5ODIzNzQ1.XL8HKg.xoNsBWj1cVhyN3gMz34gVFb2EdE').strip('"'), bot=True) 
